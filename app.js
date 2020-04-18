23b9e59b894c00441b9bfef426087d6196ef96f7
Fatiu Isiaka <fatiuisiaka@MacBook-Pro.local>
2020-04-18 19:05:41 +0100

    class-1 18-04-2020

---
+++ app.js
@@ -0,0 +1,25 @@
+// //Load HTTP module
+const http = require("http");
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+   res.json({
+     "text": "plain",
+     "number": 56
+   });
+});
+app.post('/sample-post', (req, res) => {
+   res.json(req.body);
+});
+
+
+const server = http.createServer(app)
+module.exports = server
